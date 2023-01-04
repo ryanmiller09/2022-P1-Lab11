355842345b89320b24f03a9f0420c8fc111b527d
13195 <ryan.miller@vtsd.com>
2023-01-04 10:07:08 -0500

    Lab 11

--- Lab11avst.java
+++ Lab11avst.java
@@ -22,16 +22,30 @@ public class Lab11avst
     public static void computePrimes(boolean primes[])
     {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
-        int prime[] = new int[100];
 
-        for(int k = 0; k < prime.length; k++) {
-            System.out.println(prime[k]);
+        for(int k = 2; k < primes.length; k++) {
+            primes [k] = true;
         }
-        for(int k = 0; k < prime.length; k++){
-            prime[k] = 1;
+
+       for(int x = 4; x < primes.length; x+=2){
+           primes[x] = false;
+       }
+
+       for(int y = 6; y < primes.length; y+=3){
+           primes[y] = false;
+       }
+
+       for(int g = 10; g < primes.length; g+=5){
+           primes[g] = false;
+       }
+
+       for(int p = 14; p < primes.length; p+=7){
+
+       }
+
 
           }
-        }
+
 
 
 
@@ -41,6 +55,14 @@ public class Lab11avst
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
 
+        for(int k = 2; k < primes.length; k++) {
+            if(primes[k] == true){
+                System.out.print(k + " ");
+            }
+
+
+
+        }
     }
 
 }
